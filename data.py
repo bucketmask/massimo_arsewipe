97e218272eb4e7f6c2d96bf61ed31e699dea4b5c
bucketmask <43216179+bucketmask@users.noreply.github.com>
2020-03-21 11:12:16 +0000

    sd\

--- data.py
+++ data.py
@@ -2,4 +2,4 @@ class characters :
     all_characters = []
     class jessica :
         name = "jessica"
-        description = open("../characters/jessica_description")
+        description = open()
